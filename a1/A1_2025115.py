b3fe4df891e13c9ac9d688351cb60d048ef7a203
Jan-Niklas Schneider <heisse.cola@gmail.com>
2019-02-21 12:08:14 +0100

    Fixed it all up

--- a1/A1_2025115.py
+++ a1/A1_2025115.py
@@ -5,17 +5,20 @@ import numpy as np
 df = pd.read_excel('fld.xlsx')
 
 matrix = df.values
+# print(matrix)
 locs_by_name = list(df.columns)
 locations = []
 locations.extend(range(1, 38))
 capacity = 4000
 demand = 100
-fix_cost = 100000
+fix_cost1 = 100000
+fix_cost2 = 150000
 
+# create a dictionary of dictionary that contains all costs for any location
+# i.e. {1: {1: 0, 2: 160, 3: 3082, 4: 1639, ....
 costs = {}
 for i in locations:
     costs[i] = {}
-
 for (x,y), value in np.ndenumerate(matrix):
     key = x+1
     inner_key = y+1
@@ -28,8 +31,6 @@ y = LpVariable.dicts('y', locations, 0, 1, LpBinary)
 x = LpVariable.dicts('x',
                         [(i, j) for i in locations
                          for j in locations], 0)
-# c_ij
-c = LpVariable.dicts('c', costs, 0)
 # s_i - capacity
 s = LpVariable.dicts("s", locations, 0, capacity)
 # d_j - demand
@@ -39,13 +40,14 @@ f = dict()
 d = dict()
 s = dict()
 for k in locations:
-    f[k] = fix_cost
+    f[k] = fix_cost1
     d[k] = demand
     s[k] = capacity
 
-fy = lpSum(y[i]*f[i] for i in locations)
-cx = lpSum(costs[i][j] * x[(i, j)] for i in locations for j in locations)
-objective_function = lpSum(y[i]*f[i] for i in locations) + lpSum(costs[i][j] * x[(i, j)] for i in locations for j in locations)
+# fy = lpSum(y[i]*f[i] for i in locations)
+# cx = lpSum(costs[i][j] * x[(i, j)] for i in locations for j in locations)
+objective_function = lpSum(y[i]*f[i] for i in locations) + \
+                     lpSum(costs[i][j] * x[(i, j)] for i in locations for j in locations)
 
 # objective function
 model += lpSum(objective_function)
@@ -55,14 +57,18 @@ for i in locations:
     model += lpSum(x[(i, j)] for j in locations) == d[i]
 
 for i in locations:
-    model += lpSum(x[(i, j)] for j in locations) <= y[i]*s[i]
+    for j in locations:
+        model += lpSum(x[(i, j)]) <= y[j]*s[j]
 
 model.solve()
 print(LpStatus[model.status])
 
 TOL = 0.00001
+nr_of_dcs = 0
 for i in locations:
     if y[i].varValue > TOL:
-        print("Location ", i, " produces ", [j for j in locations if x[(i, j)].varValue > TOL])
+        nr_of_dcs += 1
+        print("DC in", locs_by_name[i-1])
+print("Nr. of DC's set up is {}".format(nr_of_dcs))
 
 
