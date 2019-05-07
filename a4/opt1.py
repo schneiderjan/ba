33ae0274b22776b6e33f9c057f9f5ab992598918
Jan-Niklas Schneider <heisse.cola@gmail.com>
2019-05-07 18:01:12 +0200

    Im figuring things out

--- a4/opt1.py
+++ a4/opt1.py
@@ -2,15 +2,17 @@ from pulp import *
 import pandas as pd
 import numpy as np
 
-data =  pd.read_excel('RUL_consultancy_predictions.xlsx')
+data = pd.read_excel('RUL_consultancy_predictions.xlsx')
 
 # nr of engines
 N = 100
-nr_teams = 2
-danger = 25
+nr_teams_a = 2
+nr_teams_b = 2
+nr_teams = 4
+T = 25
 # T = t_p
 
-#variables
+# variables
 c = dict()
 mu_A = dict()
 mu_B = dict()
@@ -38,21 +40,54 @@ for i in range(N):
 
 for i in range(N):
     if 0 <= i <= 32 or 67 <= i <= 99:
-        mu_A[i] = mu_A[i] + 1
+        mu_B[i] = mu_A[i] + 1
     elif 33 <= i <= 66:
-        mu_A[i] = mu_A[i] + 2
+        mu_B[i] = mu_A[i] + 2
 
 for i in range(N):
     rul[i] = data.at[i, 'RUL']
 
-c_j = LpVariable.dicts("c_j", range(N), 0, c)
-mu_A_j = LpVariable.dicts("mu_A_j", range(N), 0, mu_A)
-mu_B_j = LpVariable.dicts("mu_B_j", range(N), 0, mu_B)
-x_ij = LpVariable.dicts('x_ij', [(i, j) for i in range(N) for j in range(N)], 0)
-t = LpVariable.dicts('t', range(danger), lowBound=0, upBound=danger, cat='integer')
-f_j = LpVariable.dicts('f_j', range(N), 0, 1, LpBinary)
+# for i in range(N):
 
-#model
+
+
+# print(mu_A)
+# print(mu_B)
+#
+c = LpVariable.dicts("c_j", range(N), 0, c)
+mu = LpVariable.dicts("mu", range(N), 0, mu_A)
+x = LpVariable.dicts('x', [(i, j, t) for i in range(N) for j in range(N) for t in range(T)], 0, cat='integer')
+s = LpVariable.dicts('s', [(i, j, t) for i in range(N) for j in range(N) for t in range(T)], 0, cat='integer')
+t = LpVariable.dicts('t', range(T), lowBound=0, upBound=T, cat='integer')
+f = LpVariable.dicts('f_j', range(N), 0, 1, cat='integer')
+d= LpVariable.dicts('d_j', range(N), lowBound=0, upBound=T, cat='integer')
+e = LpVariable.dicts('e_j', range(N), 0, cat='integer')
+# model
 model = LpProblem("opt1_B", LpMinimize)
 
-objective_function = lpSum(c_j[i] * max(0, () ) for i in range(N))
+print(x)
+#objective function
+# objective_function =
+model += lpSum(e[j] for j in range(N))
+
+#constraints
+for j in range(N):
+    for i in range(nr_teams):
+        model += lpSum(x[(i, j, t)] for t in range(T)) <= 1
+
+for t in range(T):
+    for j in range(N):
+        model += lpSum(s[(i, j, t)]) <= 1
+
+for t in range(T):
+    for j in range(N):
+        model += lpSum(t * s[(i,j,t)] + mu[(i,j)] for i in range(nr_teams)) <= T # missing:  + mu
+
+
+# for j in range(N):
+#     model += lpSum(f[j]) <= 1
+
+status = model.solve()
+print(LpStatus[model.status])
+
+# status= model.solve(pulp.PULP_CBC_CMD(maxSeconds=60))
