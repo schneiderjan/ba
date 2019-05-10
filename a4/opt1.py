2fd79b30cc4c57c28d8f8442486e133557cfd9a0
Jan-Niklas Schneider <heisse.cola@gmail.com>
2019-05-10 19:23:51 +0200

    Shit just wont work

--- a4/opt1.py
+++ a4/opt1.py
@@ -6,10 +6,10 @@ data = pd.read_excel('RUL_consultancy_predictions.xlsx')
 
 print('Preparing values')
 # nr of engines
-N = 100
-nr_teams_a = 2
-nr_teams_b = 2
-nr_teams = 4
+M = 100
+I_a = 2
+I_b = 2
+I = 4
 T = 25
 # T = t_p
 teams = {0: 'a', 1: 'a', 2: 'b', 3: 'b'}
@@ -20,7 +20,7 @@ mu_A = dict()
 mu_B = dict()
 rul = dict()
 
-for i in range(N):
+for i in range(M):
     if 0 <= i <= 19 or 60 <= i <= 79:
         cost[i] = 5
     elif 20 <= i <= 39:
@@ -30,7 +30,7 @@ for i in range(N):
     elif 80 <= i <= 99:
         cost[i] = 3
 
-for i in range(N):
+for i in range(M):
     if 0 <= i <= 24:
         mu_A[i] = 4
     elif 25 <= i <= 49:
@@ -40,72 +40,77 @@ for i in range(N):
     elif 75 <= i <= 99:
         mu_A[i] = 5
 
-for i in range(N):
+for i in range(M):
     if 0 <= i <= 32 or 67 <= i <= 99:
         mu_B[i] = mu_A[i] + 1
     elif 33 <= i <= 66:
         mu_B[i] = mu_A[i] + 2
 
-for i in range(N):
+for i in range(M):
     rul[i] = data.at[i, 'RUL']
 
 mu = {'a': mu_A, 'b': mu_B}
 
-c = LpVariable.dicts("c", range(N), 0, cat='integer')
-x = LpVariable.dicts('x', indexs=[(i, j, t) for i in range(N) for j in range(N) for t in range(T)], lowBound=0, cat='binary')
-# s = LpVariable.dicts('s', [(i, j, t) for i in range(N) for j in range(N) for t in range(T)], 0, cat='integer')
+c = LpVariable.dicts("c", range(M), 0)
+x = LpVariable.dicts('x', indexs=[(i, j, t) for i in range(I) for j in range(M) for t in range(T)], lowBound=0, upBound=1,
+                     cat='binary')
+# s = LpVariable.dicts('s', [(i, j, t) for i in range(M) for j in range(M) for t in range(T)], 0, cat='integer')
 # t = LpVariable.dicts('t', range(T), lowBound=0, upBound=T, cat='integer')
-# f = LpVariable.dicts('f_j', range(N), 0, 1, cat='integer')
+# f = LpVariable.dicts('f_j', range(M), 0, 1, cat='integer')
 
 print('Creating model')
 
 # model
 model = LpProblem("opt1_B", LpMinimize)
-objective_function = lpSum(c[j] for j in range(N))
+objective_function = lpSum(c[j] for j in range(M))
 model += objective_function
 
 print('Adding constraints')
 
 # constraints
 # team to engine
-for j in range(N):
-    for i in range(nr_teams):
-        model += lpSum(x[(i, j, t)] for t in range(T)) <= 1
-
-# one team only on one engine at given time.
-for i in range(nr_teams):
-    team = teams[i]
-    for j in range(N):
-        for t in range(T):
-            model += lpSum(x[(i, g, h)] for h in range(t, min(T, t + mu[teams[i]][j] - 1)) for g in range(N))  \
-                     <= T - t - rul[j]
-                     # <= N * T * (x[(i, j, t)])
+for j in range(M):
+    model += lpSum(x[(i, j, t)] for i in range(I) for t in range(T)) <= 1
 
 # a team must finish their work within T
-for i in range(nr_teams):
-    for j in range(N):
-        model += lpSum([(t + mu[teams[i]][j]) * x[(i, j, t)] for t in range(T)]) <= T
+for i in range(I):
+    for j in range(M):
+        for t in range(T):
+            model += lpSum((t + mu[teams[i]][j]) * x[(i, j, t)]) <= T
 
 # cost constraint
-for j in range(N):
-    available_days = T - rul[j]
-    model += cost[j] * (available_days - lpSum(((x[(i, j, t)]) * (T - t - mu[teams[i]][j])) for t in range(T)
-                                               for i in range(nr_teams))) == c[j]
+for i in range(I):
+    for j in range(M):
+        available_days = T - rul[j]
+        model += cost[j] * (available_days - lpSum(((x[(i, j, t)]) * (T - t - mu[teams[i]][j])) for t in range(T))) == c[j]
+
+# should be working - solution infeasible
+# with more time get negative result and not solved.
+# team blocker
+for i in range(I):
+    for j in range(M):
+        for t in range(T):
+            model += lpSum(x[(i, w, e)] for w in range(M) for e in range(t, min(T, t + mu[teams[i]][w] - 1)))  \
+                      <= T * (1 - x[(i, j, t)])
+
+# # team blocker - instant infeasible
+# for i in range(I):
+#     for t in range(T):
+#         model += lpSum(x[(i, w, e)] for w in range(M) for e in range(max(1, t + mu[teams[i]][w] + 1), T)) <= 1
 
 # cost is either 0 or higher
-for j in range(N):
+for j in range(M):
     model += c[j] >= 0
 
 print('Solving model')
-status = model.solve(pulp.PULP_CBC_CMD(maxSeconds=60))
+status = model.solve(pulp.PULP_CBC_CMD(maxSeconds=120))
 # status = model.solve()
 print(LpStatus[model.status])
 
 print("Total cost: {}".format(pulp.value(model.objective)))
 TOL = 0.00001
-nr_of_dcs = 0
 for j in model.variables():
-    if j.varValue > 0:
+    if j.varValue > TOL:
         print(j.name + " = " + str(j.varValue))
 
 # constraints needed:
@@ -115,3 +120,23 @@ for j in model.variables():
 # [x] assign costs
 # [] fail only once \\ might be not needed anymore.
 # []
+
+
+# # one team only on one engine at given time.
+# for i in range(I):
+#     team = teams[i]
+#     for j in range(M):
+#         for t in range(T):
+#             model += lpSum(x[(i, g, h)] for h in range(t, min(T, t + mu[teams[i]][j] - 1)) for g in range(M))  \
+#                      <= T - t - rul[j]
+#                      # <= M * T * (x[(i, j, t)])
+
+# for i in range(I):
+#     for j in range(M):
+#         for t in range(T):
+#             model += lpSum(x[(i, w, e)] - 1 for w in range(M) for e in range(t, min(T, t + mu[teams[i]][j] - 1))) <= T(1 - x[(i, j, t)])
+# team blocker
+# for i in range(I):
+#     for j in range(M):
+#         for t in range(T):
+#             model += lpSum(x[(i, k, d)] for d in range(min(T, t + mu[teams[i]][j])) for k in range(M)) <=
